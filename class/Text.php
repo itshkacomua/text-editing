b84468d51b2eb0f9c547aaf781a540ef9272e83d
Andrey <progandrey@gmail.com>
2019-07-01 16:19:42 +0300

    edit class Text

--- class/Text.php
+++ class/Text.php
@@ -10,17 +10,12 @@ class Text
      */
     protected $text;
 
-    public function __construct(string $text)
-    {
-        $this->text = $text;
-    }
-
     /**
      * Set text
      *
      * @param string $text
      */
-    public function set(string $text)
+    public function set(string $text): void
     {
         $this->text = $text;
     }
@@ -46,5 +41,58 @@ class Text
         } else {
             return $this->text;
         }
+    }
+
+    /**
+     * Text clipping method
+     * @param $date_begin
+     * @param $date_end
+     */
+    function СlippingЕext($text, $strlen, $params = ['clipping' => 'y', 'three_dots' => 'y'])
+    {
+        //$text=strip_tags($text);
+        //$text=htmlspecialchars($text);
+        $text_strlen = mb_strlen($text);
+        $arr_test_elements = ['b', 'span', 'p', 'i', 'div'];
+
+        if ($params['clipping'] == 'y' && $text_strlen > $strlen) {
+            $pos = mb_strpos($text, ' ', $strlen);
+
+            if ($pos > 0)
+                $return = mb_substr($text, 0, $pos);
+            else
+                $return = $text;
+            //$return = mb_substr($text, 0, strrpos(substr(htmlspecialchars($text),0, $strlen), ' '));
+        } else
+            $return = $text;
+
+        if ($params['three_dots'] == 'y' && $text_strlen > $strlen)
+            $return .= ' ...';
+
+        /**
+         * checks unopened items and closes them as needed.
+         */
+        foreach ($arr_test_elements AS $value) {
+            $element = substr_count($return, '<' . $value . '>') - substr_count($return, '</' . $value . '>');
+            if ($element > 0)
+                $return .= self::GetSeveralHTMLElements($value, $element);
+        }
+
+        return $return;
+    }
+
+    /**
+     * Method of adding closing elements
+     * @param $element
+     * @param int $number
+     * @return string
+     */
+    function GetSeveralHTMLElements($element, $number = 1)
+    {
+        $result = '';
+        for ($i = 0; $i < $number; $i++)
+            $result .= '</' . $element . '>';
+
+        return $result;
     }
 }
--- example.php
+++ example.php
@@ -4,6 +4,11 @@
 use Text\Text;
 
 $text = 'Жена посылает программиста за продуктами: — Купи палку колбасы, и если будут яйца, возьми десяток. Программист в магазине: — Дайте мне, пожалуйста, палку колбасы. Спасибо. А яйца у вас есть? — Есть. — Тогда ещё девять палок колбасы.';
-$obj = new Text($text);
+$obj = new Text();
+$obj->set($text);
 //echo $obj->get();
 echo $obj->TextClipping(22, '..');
+
+$obj_num = new Converter();
+echo '<br>' . $obj_num->romanNumbersInArabic('I');
+echo '<br>' . $obj_num->arabicNumbersInRoman(6);
